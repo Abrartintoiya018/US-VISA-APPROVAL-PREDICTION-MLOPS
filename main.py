9a16908265d6a76b13397664cb9648a3cd45f745
Abrar tintoiya <mohammadabrartintoiya@gmail.com>
2025-09-22 22:25:27 +0530

    mongodb demo ended

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from us-visa-approval-prediction-mlops!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "us_visa",
+    version = "0.0.0",
+    author = "Abrar tintoiy",
+    author_email = "mohammadabrartintoiya@gmail.com",
+    packages = find_packages()
+)
